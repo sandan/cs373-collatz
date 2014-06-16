64b6a49f0e19fec9fb93c88ca5979eabe65e238a
Mark <mas5774@cs.utexas.edu>
2014-06-16 06:22:18 -0500

    I implemented the first optimization. This resolves #14

--- Collatz.py
+++ Collatz.py
@@ -60,9 +60,10 @@ def cycle_length (n) :
     while n > 1 :
         if (n % 2) == 0 :
             n = (n // 2)
-        else :
-            n = (3 * n) + 1
-        c += 1
+            c+=1
+        else : #calculates (3n+1)/2
+            n = n + (n>>1) + 1
+            c += 2
     assert c > 0
     return c
 
